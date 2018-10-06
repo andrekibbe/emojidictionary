bbc63983887abeb69dba62fdc39975ec9722ce38
Andre Kibbe <akibbe02@gmail.com>
2018-10-06 10:08:57 -0700

    Finished application

--- Emoji Dictionary/DefinitionViewController.swift
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -10,36 +10,22 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
     
+    @IBOutlet weak var birthYearLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var definitionLabel: UILabel!
     @IBOutlet weak var emojiLabel: UILabel!
     
-    var emoji = "NO EMOJI"
+    var emoji = Emoji()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        emojiLabel.text = emoji
-        
-        if emoji == "🐹" {
-            definitionLabel.text = "A cute Hamster!"
-        }
-        if emoji == "😎" {
-            definitionLabel.text = "A dude with sunglasses"
-        }
-        if emoji == "🐹" {
-            definitionLabel.text = "A cute Hamster!"
-        }
-        if emoji == "🐹" {
-            definitionLabel.text = "A cute Hamster!"
-        }
-        if emoji == "🐹" {
-            definitionLabel.text = "A cute Hamster!"
-        }
-        if emoji == "🐹" {
-            definitionLabel.text = "A cute Hamster!"
-        }
+        emojiLabel.text = emoji.stringEmoji
+        birthYearLabel.text = "Origination Date: \(emoji.birthYear)"
+        categoryLabel.text = "Category: \(emoji.category)"
+        definitionLabel.text = emoji.definition
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Emoji.swift
@@ -0,0 +1,14 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Andre Kibbe on 10/3/18.
+//  Copyright © 2018 Chez Andre. All rights reserved.
+//
+
+class Emoji {
+    var stringEmoji = ""
+    var definition  = ""
+    var category    = ""
+    var birthYear   = 0
+}
